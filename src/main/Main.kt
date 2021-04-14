a757a21585c6c3f7f545805d2bb165dd61b75c36
Euan Imperial <es1519@ic.ac.uk>
2021-04-14 21:21:43 +0100

    Finished the Plugboard.kt class

---
+++ src/main/Main.kt
@@ -0,0 +1,5 @@
+
+fun main(args: Array<String>) {
+    val plugboard = Plugboard("AL FG WE TR, PQ")
+    println(plugboard.getWiring())
+}
---
+++ src/main/Plugboard.kt
@@ -0,0 +1,63 @@
+class Plugboard(connections: String) {
+
+
+    private val wirings: MutableList<Int>
+
+    companion object {
+        private const val A = 65
+    }
+
+    init {
+        val pieces = connections.split(" ")
+        assert(pieces.isNotEmpty())
+        pieces.forEach { assert(it.length == 2) }
+        wirings = decodeWirings(pieces)
+    }
+
+    private fun identityWirings(): MutableList<Int> {
+        val wires = mutableListOf<Int>()
+        for (i in 0..25) {
+            wires.add(i)
+        }
+        return wires
+    }
+
+    private fun decodeWirings(connections: List<String>): MutableList<Int> {
+        val wires = identityWirings()
+        val usedChars = HashSet<Int>()
+
+        for (pair in connections) {
+            val c1 = pair[0].toInt() - A
+            val c2 = pair[1].toInt() - A
+
+            when {
+                c1 == c2 -> throw Error("Cannot plug into oneself")
+                usedChars.contains(c1) -> throw Error("Plug ${pair[0]} already plugged in")
+                usedChars.contains(c2) -> throw Error("Plug ${pair[1]} already plugged in")
+            }
+
+            usedChars.add(c1)
+            usedChars.add(c2)
+
+            wires[c1] = c2
+            wires[c2] = c1
+        }
+        return wires
+    }
+
+    fun encode(value: Int): Int {
+        assert(value in 0..25)
+        return wirings[value]
+    }
+
+    fun encode(value: Char): Char {
+        val ascii = value.toInt() - A
+        assert(ascii in 0..25)
+        return (wirings[ascii] + A).toChar()
+    }
+
+    fun getWiring(): MutableList<Int> {
+        return wirings
+    }
+
+}
